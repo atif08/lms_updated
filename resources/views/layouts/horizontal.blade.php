76671da6a55100c8c26a98468eeac5f3bdc96cd2
Nouman <nouman.aslam@sixlogics.com>
2026-03-18 13:58:02 +0500

    push pro media package

--- resources/views/layouts/horizontal.blade.php
+++ resources/views/layouts/horizontal.blade.php
@@ -5,19 +5,19 @@
             <div class="navbar-brand-box">
                 <a href="{{route('home')}}" class="logo logo-dark">
                     <span class="logo-sm">
-                        <img src="{{ URL::asset('images/logo-sm.png') }}" alt="" height="60">
+{{--                        <img src="{{ URL::asset('images/logo-sm.png') }}" alt="" height="60">--}}
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ URL::asset('images/logo-dark.png') }}" alt="" height="60">
+{{--                        <img src="{{ URL::asset('images/logo-dark.png') }}" alt="" height="60">--}}
                     </span>
                 </a>
 
                 <a href="{{route('home')}}" class="logo logo-light">
                     <span class="logo-sm">
-                        <img src="{{ URL::asset('images/logo-sm.png') }}" alt="" height="60">
+{{--                        <img src="{{ URL::asset('images/logo-sm.png') }}" alt="" height="60">--}}
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ URL::asset('images/logo-light.png') }}" alt="" height="60">
+{{--                        <img src="{{ URL::asset('images/logo-light.png') }}" alt="" height="60">--}}
                     </span>
                 </a>
             </div>
